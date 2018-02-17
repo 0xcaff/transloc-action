7df69c2baa9cc8a805649cfacbdf2670b94e4ccf
Martin Charles <martincharles07@gmail.com>
2018-02-16 21:22:43 -0500

    Updated lifespan
    I'm not sure why this isn't working.

--- src/helperHandler.js
+++ src/helperHandler.js
@@ -26,7 +26,7 @@ export const recordContext = (
   args: Object,
   app: DialogflowApp
 ): void => {
-  const lifespan = 1;
+  const lifespan = 2;
   app.setContext(handler, lifespan, {
     handler,
     originalArguments: args
