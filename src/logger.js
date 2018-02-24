e5ff1e7947082706b19ab4e869e0708d30144b8d
Martin Charles <martincharles07@gmail.com>
2018-02-24 11:49:10 -0500

    Removed Stackdriver Logging
    StackDriver logging was added to provide structured logging. Apparently,
    it wasn't really tested in the context of Google Cloud Functions. We
    were experiencing a couple of issues:
    
    * Random Crashing (#25)
    
    * Missing Logs
      The function would sometimes exit without sending all logs.
    
    * Missing Tags
      The previous logging implementation didn't provide tags with the
      function execution id. Standard output logs do this automatically.
    
    While structured logging is great, the costs outweigh the benefits in
    this case (unfortunately). So I'm switching back to stdout/stderr based
    text logs.

--- src/logger.js
+++ src/logger.js
@@ -1,14 +1,10 @@
 import path from "path";
 import bunyan from "bunyan";
-import LoggingBunyan from "@google-cloud/logging-bunyan";
 
 const streams = [];
 
 if (process.env.NODE_ENV === "production") {
-  // Creates a Bunyan Stackdriver Logging client
-  const loggingBunyan = new LoggingBunyan();
-
-  streams.push(loggingBunyan.stream());
+  streams.push({ level: "debug", stream: process.stdout });
 } else {
   const bunyanDebugStream = require("bunyan-debug-stream");
   const stream = bunyanDebugStream({
