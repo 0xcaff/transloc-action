7586a7c3e8ece3246a75cb581b723ad702a1a271
Martin Charles <martincharles07@gmail.com>
2018-02-24 12:53:14 -0500

    Simpler Logging
    KISS.
    
    Closes #37

--- src/logger.js
+++ src/logger.js
@@ -1,56 +1,29 @@
-import { Writable } from "stream";
-import path from "path";
-import bunyan from "bunyan";
-
-const streams = [];
-
-type Entry = {
-  level: string
+// @flow
+const normalizeFirst = (first: Object | string): string => {
+  if (typeof first === "object") {
+    // JSON.stringify is needed because otherwise only the first few levels
+    // of the object are printed.
+    return JSON.stringify(first);
+  } else {
+    return first;
+  }
 };
 
-// Gets the function which the message will be logged to for the log entry.
-const getLogger = (entry: Entry) => {
-  if (entry.level === bunyan.WARN) {
-    // eslint-disable-next-line no-console
-    return console.warn;
+const normalizeSecond = (second: ?string): string => {
+  if (second) {
+    return second;
   }
 
-  // eslint-disable-next-line no-console
-  return console.log;
+  return "";
 };
 
-// A stream which consumes objects and delivers them to console.log /
-// console.warn.
-class ConsoleWritableStream extends Writable {
-  constructor() {
-    super({ objectMode: true });
+const logger = {
+  info(first: Object | string, second: ?string) {
+    const normalizedFirst = normalizeFirst(first);
+    const normalizedSecond = normalizeSecond(second);
+
+    console.log(normalizedFirst, normalizedSecond);
   }
-
-  _write(entry: Entry, encoding: string, done: void => void) {
-    const logger = getLogger(entry);
-
-    logger(JSON.stringify(entry));
-
-    done();
-  }
-}
-
-if (process.env.NODE_ENV === "production") {
-  const stream = new ConsoleWritableStream();
-  streams.push({ level: "info", type: "raw", stream });
-} else {
-  const bunyanDebugStream = require("bunyan-debug-stream");
-  const stream = bunyanDebugStream({
-    basepath: path.resolve(__dirname, "..")
-  });
-
-  streams.push({ level: "info", type: "raw", stream });
-}
-
-const logger = bunyan.createLogger({
-  name: "fulfilment-handler",
-  streams,
-  serializers: bunyan.stdSerializers
-});
+};
 
 export default logger;
