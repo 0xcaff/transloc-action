fc4ca004fcfb002d6b08bbf47d50eab4a3b999a9
Martin Charles <martincharles07@gmail.com>
2018-02-25 11:05:46 -0500

    Prettify Fast Levenshtein Typing

--- flow-typed/npm/fast-levenshtein_vx.x.x.js
+++ flow-typed/npm/fast-levenshtein_vx.x.x.js
@@ -1,12 +1,12 @@
 // flow-typed signature: efb4ebc4464e1aa687650e0487e08e2d
 // flow-typed version: <<STUB>>/fast-levenshtein_v^2.0.6/flow_v0.65.0
 
-declare module 'fast-levenshtein' {
+declare module "fast-levenshtein" {
   declare export type Options = {
-    useCollator: boolean,
+    useCollator: boolean
   };
 
   declare module.exports: {
-    get: (str1: string, str2: string, options?: Options) => number;
-  }
+    get: (str1: string, str2: string, options?: Options) => number
+  };
 }
